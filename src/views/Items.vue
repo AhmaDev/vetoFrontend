bd9d6a0c6aaea0a87e255d3a2600dd24316ed5bd
AhmaDev <ahmadwig@gmail.com>
2024-08-05 18:46:41 +0300

    fix

--- src/views/Items.vue
+++ src/views/Items.vue
@@ -3,11 +3,7 @@
     <v-app-bar app>
       <v-toolbar-title>المواد</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        v-if="checkPermission('items_add')"
-        @click="newItemDialog = true"
-        icon
-      >
+      <v-btn v-if="checkPermission('items_add')" @click="newItemDialog = true" icon>
         <v-icon>mdi-plus</v-icon>
       </v-btn>
     </v-app-bar>
@@ -15,81 +11,42 @@
     <v-card class="pa-10">
       <v-row>
         <v-col>
-          <v-autocomplete
-            v-model="selectedGroup"
-            item-text="itemGroupName"
-            item-value="idItemGroup"
-            :items="groups"
-            outlined
-            label="فرز حسب المجموعات"
-            clearable
-          ></v-autocomplete>
+          <v-autocomplete v-model="selectedGroup" item-text="itemGroupName" item-value="idItemGroup" :items="groups"
+            outlined label="فرز حسب المجموعات" clearable></v-autocomplete>
         </v-col>
         <v-col>
-          <v-autocomplete
-            v-model="selectedBrand"
-            item-text="brandName"
-            item-value="idBrand"
-            :items="brands"
-            outlined
-            label="فرز حسب المورد"
-            clearable
-            @change="searchBrand"
-          ></v-autocomplete>
+          <v-autocomplete v-model="selectedBrand" item-text="brandName" item-value="idBrand" :items="brands" outlined
+            label="فرز حسب المورد" clearable @change="searchBrand"></v-autocomplete>
         </v-col>
         <v-col>
-          <v-autocomplete
-            v-model="itemNameSearch"
-            item-text="fullItemName"
-            item-value="idItem"
-            :items="allItems"
-            outlined
-            label="فرز حسب اسم المادة"
-            clearable
-            @change="searchItemName"
-          ></v-autocomplete>
+          <v-autocomplete v-model="itemNameSearch" item-text="fullItemName" item-value="idItem" :items="allItems"
+            outlined label="فرز حسب اسم المادة" clearable @change="searchItemName"></v-autocomplete>
         </v-col>
         <v-col>
-          <v-autocomplete
-            item-text="sellPriceName"
-            item-value="idSellPrice"
-            :items="sellPrices"
-            outlined
-            label="فرز حسب السعر"
-            clearable
-            v-model="selectedSellPrice"
-          ></v-autocomplete>
+          <v-autocomplete item-text="sellPriceName" item-value="idSellPrice" :items="sellPrices" outlined
+            label="فرز حسب السعر" clearable v-model="selectedSellPrice"></v-autocomplete>
+        </v-col>
+        <v-col>
+          <v-select v-model="isAvailable" outlined :items="['الكل', 'متاح', 'غير متاح']"></v-select>
         </v-col>
       </v-row>
     </v-card>
-    <div
-      v-for="group in selectedGroup == null
+    <div v-for="group in selectedGroup == null
         ? groups
-        : groups.filter((e) => e.idItemGroup == selectedGroup)"
-      :key="group.idItemGroup"
-    >
+        : groups.filter((e) => e.idItemGroup == selectedGroup)" :key="group.idItemGroup">
       <h3>{{ group.itemGroupName }}</h3>
       <br />
       <v-row>
-        <v-col
-          cols="3"
-          v-for="item in items.filter(
-            (item) => item.itemGroupId == group.idItemGroup
-          )"
-          :key="item.idItem"
-        >
+        <v-col cols="3" v-for="item in isAvailable == 'الكل' ? items.filter(
+        (item) => item.itemGroupId == group.idItemGroup
+      ) : isAvailable == 'متاح' ? items.filter(
+        (item) => item.itemGroupId == group.idItemGroup && item.isAvailable == 1
+      ) : items.filter(
+        (item) => item.itemGroupId == group.idItemGroup && item.isAvailable == 0
+      )" :key="item.idItem">
           <v-card elevation="6">
-            <v-img
-              v-if="item.imagePath != null"
-              height="200"
-              :src="$baseUrl + 'files/' + item.imagePath"
-            ></v-img>
-            <img
-              v-if="item.imagePath == null"
-              src="@/assets/no_image_placeholder.png"
-              height="250"
-              width="100%"
-            />
+            <v-img v-if="item.imagePath != null" height="200" :src="$baseUrl + 'files/' + item.imagePath"></v-img>
+            <img v-if="item.imagePath == null" src="@/assets/no_image_placeholder.png" height="250" width="100%" />
             <div class="itemNotAvailible" v-if="item.isAvailable == 0">
               <br />
               <i style="font-size: 40px" class="las la-eye-slash"></i>
@@ -100,49 +57,32 @@
               {{ item.fullItemName }}
             </div>
 
-            <v-card-text
-              v-if="item.prices[0] != null"
-              class="textOverFlow green--text headline"
-            >
+            <v-card-text v-if="item.prices[0] != null" class="textOverFlow green--text headline">
               {{
-                (item.prices.filter((e) => e.sellPriceId == selectedSellPrice)
-                  .length > 0
-                  ? item.prices.filter(
-                      (e) => e.sellPriceId == selectedSellPrice
-                    )[0].price
-                  : 0
-                ).toLocaleString()
-              }}
+        (item.prices.filter((e) => e.sellPriceId == selectedSellPrice)
+          .length > 0
+          ? item.prices.filter(
+            (e) => e.sellPriceId == selectedSellPrice
+          )[0].price
+          : 0
+        ).toLocaleString()
+      }}
               د.ع
             </v-card-text>
             <v-card-actions>
-              <v-btn
-                :disabled="!checkPermission('items_edit')"
-                icon
-                target="_BLANK"
-                :to="'/item/' + item.idItem"
-              >
+              <v-btn :disabled="!checkPermission('items_edit')" icon target="_BLANK" :to="'/item/' + item.idItem">
                 <v-icon :title="item.itemName">mdi-magnify</v-icon>
               </v-btn>
               <v-spacer></v-spacer>
               <v-menu offset-y>
                 <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    :disabled="!checkPermission('items_delete')"
-                    v-bind="attrs"
-                    v-on="on"
-                    icon
-                  >
-                    <v-icon color="red" title="حذف المادة"
-                      >mdi-delete-outline</v-icon
-                    >
+                  <v-btn :disabled="!checkPermission('items_delete')" v-bind="attrs" v-on="on" icon>
+                    <v-icon color="red" title="حذف المادة">mdi-delete-outline</v-icon>
                   </v-btn>
                 </template>
                 <v-list>
                   <v-list-item @click="deleteItem(item.idItem)">
-                    <v-list-item-title
-                      >اضغط هنا لتأكيد حذف المادة</v-list-item-title
-                    >
+                    <v-list-item-title>اضغط هنا لتأكيد حذف المادة</v-list-item-title>
                   </v-list-item>
                 </v-list>
               </v-menu>
@@ -160,212 +100,91 @@
         <v-card-text>
           <v-row justify="center" align="center">
             <v-col cols="6">
-              <v-text-field
-                label="رمز المادة"
-                v-model="newItem.itemCode"
-                outlined
-                hide-details
-                dense
-              ></v-text-field>
+              <v-text-field label="رمز المادة" v-model="newItem.itemCode" outlined hide-details dense></v-text-field>
             </v-col>
             <v-col cols="6">
-              <v-text-field
-                label="باركود"
-                type="number"
-                v-model="newItem.itemBarcode"
-                outlined
-                hide-details
-                dense
-              ></v-text-field>
+              <v-text-field label="باركود" type="number" v-model="newItem.itemBarcode" outlined hide-details
+                dense></v-text-field>
             </v-col>
             <v-col cols="6">
-              <v-text-field
-                label="نوع المنتج داخل العلبة"
-                type="text"
-                v-model="newItem.itemType"
-                outlined
-                hide-details
-                dense
-              ></v-text-field>
+              <v-text-field label="نوع المنتج داخل العلبة" type="text" v-model="newItem.itemType" outlined hide-details
+                dense></v-text-field>
             </v-col>
             <v-col cols="6">
-              <v-text-field
-                label="نوع شكل العلبة"
-                v-model="newItem.itemName"
-                outlined
-                hide-details
-                dense
-              ></v-text-field>
+              <v-text-field label="نوع شكل العلبة" v-model="newItem.itemName" outlined hide-details
+                dense></v-text-field>
             </v-col>
             <v-col cols="3">
-              <v-text-field
-                label="وزن العلبة"
-                type="number"
-                outlined
-                hide-details
-                dense
-                v-model="newItem.itemWeight"
-              ></v-text-field>
+              <v-text-field label="وزن العلبة" type="number" outlined hide-details dense
+                v-model="newItem.itemWeight"></v-text-field>
             </v-col>
             <v-col cols="3">
-              <v-autocomplete
-                outlined
-                hide-details
-                dense
-                :items="itemWeightSuffixes"
-                v-model="newItem.itemWeightSuffix"
-              ></v-autocomplete>
+              <v-autocomplete outlined hide-details dense :items="itemWeightSuffixes"
+                v-model="newItem.itemWeightSuffix"></v-autocomplete>
             </v-col>
             <v-col cols="6">
-              <v-text-field
-                label="عدد القطع في الكارتون"
-                type="number"
-                outlined
-                hide-details
-                dense
-                v-model="newItem.cartonQauntity"
-              ></v-text-field>
+              <v-text-field label="عدد القطع في الكارتون" type="number" outlined hide-details dense
+                v-model="newItem.cartonQauntity"></v-text-field>
             </v-col>
             <v-col cols="4">
-              <v-select
-                :items="brands"
-                label="الماركة"
-                v-model="newItem.brandId"
-                item-text="brandName"
-                item-value="idBrand"
-                outlined
-                hide-details
-                dense
-              ></v-select>
+              <v-select :items="brands" label="الماركة" v-model="newItem.brandId" item-text="brandName"
+                item-value="idBrand" outlined hide-details dense></v-select>
             </v-col>
             <v-col cols="4">
-              <v-select
-                :items="groups"
-                label="المجموعة"
-                v-model="newItem.itemGroup"
-                item-text="itemGroupName"
-                item-value="idItemGroup"
-                outlined
-                hide-details
-                dense
-              ></v-select>
+              <v-select :items="groups" label="المجموعة" v-model="newItem.itemGroup" item-text="itemGroupName"
+                item-value="idItemGroup" outlined hide-details dense></v-select>
             </v-col>
             <v-col cols="4">
-              <v-autocomplete
-                v-model="newItem.manufactureId"
-                :items="manufactures"
-                outlined
-                hide-details
-                dense
-                label="الشركة الموردة"
-                item-text="customerName"
-                item-value="idCustomer"
-              ></v-autocomplete>
+              <v-autocomplete v-model="newItem.manufactureId" :items="manufactures" outlined hide-details dense
+                label="الشركة الموردة" item-text="customerName" item-value="idCustomer"></v-autocomplete>
             </v-col>
 
             <v-col cols="4">
-              <v-text-field
-                label="عرض الكارتون"
-                type="number"
-                outlined
-                hide-details
-                dense
-                v-model="newItem.cartonWidth"
-              ></v-text-field>
+              <v-text-field label="عرض الكارتون" type="number" outlined hide-details dense
+                v-model="newItem.cartonWidth"></v-text-field>
             </v-col>
 
             <v-col cols="4">
-              <v-text-field
-                label="ارتفاع الكارتون"
-                type="number"
-                outlined
-                hide-details
-                dense
-                v-model="newItem.cartonHeight"
-              ></v-text-field>
+              <v-text-field label="ارتفاع الكارتون" type="number" outlined hide-details dense
+                v-model="newItem.cartonHeight"></v-text-field>
             </v-col>
 
             <v-col cols="4">
-              <v-text-field
-                label="طول الكارتون"
-                type="number"
-                outlined
-                hide-details
-                dense
-                v-model="newItem.cartonLength"
-              ></v-text-field>
+              <v-text-field label="طول الكارتون" type="number" outlined hide-details dense
+                v-model="newItem.cartonLength"></v-text-field>
             </v-col>
 
             <v-col cols="4">
-              <v-text-field
-                label="عدد ايام صلاحية المادة"
-                type="number"
-                outlined
-                hide-details
-                dense
-                v-model="newItem.expireAge"
-              ></v-text-field>
+              <v-text-field label="عدد ايام صلاحية المادة" type="number" outlined hide-details dense
+                v-model="newItem.expireAge"></v-text-field>
             </v-col>
 
             <v-col cols="4">
-              <v-text-field
-                label="الحد الادنى للتنبيه"
-                type="number"
-                hint="العدد بالكراتين"
-                outlined
-                hide-details
-                dense
-                v-model="newItem.minimumStoreNotify"
-              ></v-text-field>
+              <v-text-field label="الحد الادنى للتنبيه" type="number" hint="العدد بالكراتين" outlined hide-details dense
+                v-model="newItem.minimumStoreNotify"></v-text-field>
             </v-col>
 
             <v-col cols="4">
-              <v-text-field
-                label="الحد الاعلى للتنبيه"
-                type="number"
-                hint="العدد بالكراتين"
-                outlined
-                hide-details
-                dense
-                v-model="newItem.maximumStoreNotify"
-              ></v-text-field>
+              <v-text-field label="الحد الاعلى للتنبيه" type="number" hint="العدد بالكراتين" outlined hide-details dense
+                v-model="newItem.maximumStoreNotify"></v-text-field>
             </v-col>
             <v-col cols="12">
               <h3>اضافة سعر</h3>
               <br />
               <v-row>
                 <v-col>
-                  <v-select
-                    :items="sellPrices"
-                    label="سعر البيع"
-                    item-text="sellPriceName"
-                    item-value="idSellPrice"
-                    outlined
-                    hide-details
-                    dense
-                    v-model="newItem.sellPriceId"
-                  ></v-select>
+                  <v-select :items="sellPrices" label="سعر البيع" item-text="sellPriceName" item-value="idSellPrice"
+                    outlined hide-details dense v-model="newItem.sellPriceId"></v-select>
                 </v-col>
                 <v-col>
-                  <v-text-field
-                    dense
-                    label="المبلغ"
-                    type="number"
-                    hide-details
-                    outlined
-                    v-model="newItem.sellPriceAmount"
-                  ></v-text-field>
+                  <v-text-field dense label="المبلغ" type="number" hide-details outlined
+                    v-model="newItem.sellPriceAmount"></v-text-field>
                 </v-col>
                 <v-col>
                   <v-btn @click="addNewSellPrice()"> اضافة سعر </v-btn>
                 </v-col>
                 <v-col cols="12" v-if="newItemSellPrices.length > 0">
-                  <table
-                    class="pricesTable"
-                    width="100%"
-                    border="1"
-                    cellspacing="0"
-                  >
+                  <table class="pricesTable" width="100%" border="1" cellspacing="0">
                     <thead>
                       <tr>
                         <th>نوع البيع</th>
@@ -374,10 +193,7 @@
                       </tr>
                     </thead>
                     <tbody>
-                      <tr
-                        v-for="(price, index) in newItemSellPrices"
-                        :key="price.sellPriceId"
-                      >
+                      <tr v-for="(price, index) in newItemSellPrices" :key="price.sellPriceId">
                         <td>{{ price.sellPriceName }}</td>
                         <td>{{ price.price }}</td>
                         <td>
@@ -397,20 +213,10 @@
                 <i style="font-size: 30px" class="las la-cloud-upload-alt"></i>
                 &nbsp; &nbsp; اضافة صورة المادة
               </v-btn>
-              <input
-                id="filepicker"
-                style="display: none"
-                type="file"
-                name="uploads"
-                @change="selectFiles($event)"
-                accept="image/png, image/gif, image/jpeg"
-              />
+              <input id="filepicker" style="display: none" type="file" name="uploads" @change="selectFiles($event)"
+                accept="image/png, image/gif, image/jpeg" />
               <br />
-              <img
-                v-if="newItem.image != null"
-                class="imagePreview"
-                :src="imagePreview(newItem.image)"
-              />
+              <img v-if="newItem.image != null" class="imagePreview" :src="imagePreview(newItem.image)" />
             </v-col>
           </v-row>
         </v-card-text>
@@ -450,6 +256,7 @@ export default {
     itemWeightSuffixes: ["غرام", "كيلو", "ملي", "لتر"],
     newItemSellPrices: [],
     newItemDialog: false,
+    isAvailable: "الكل",
     newItem: {
       itemName: "",
       itemCode: "",
@@ -523,13 +330,13 @@ export default {
         .then((res) => {
           this.manufactures = res.data;
         })
-        .finally(() => {});
+        .finally(() => { });
       this.$http
         .get(this.$baseUrl + "brands")
         .then((res) => {
           this.brands = res.data;
         })
-        .finally(() => {});
+        .finally(() => { });
     },
     addNewSellPrice() {
       if (this.newItem.sellPriceId != null) {
@@ -668,12 +475,14 @@ export default {
   padding: 5px;
   text-align: center;
 }
+
 .imagePreview {
   height: 150px;
   border-radius: 20px;
   box-shadow: 0px 0px 25px 0px #0000003a;
   margin: 10px;
 }
+
 .itemNotAvailible {
   height: 250px;
   width: 100%;
